a350b0650730e0dfbfccc89f674c7acf8fc263ac
tongjuan <vstj007@126.com>
2018-08-28 11:18:58 +0800

    updata origin

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>FooterGuide</div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+</style>
--- src/router/Profile/Profile.vue
+++ src/router/Profile/Profile.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>APP</div>
+  <div>Profile</div>
 </template>
 <script>
-export default {
-
-}
+export default {}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 </style>
